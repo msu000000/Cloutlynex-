3c5ce7ff0273804d7ac9b04026a1cdceff1aea37
msu000000 <msuvi000@gmail.com>
2025-08-06 14:52:25 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
