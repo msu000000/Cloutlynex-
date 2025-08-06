9a64e1f1edfa2292e350e4334cfee77365ceaa32
msu000000 <msuvi000@gmail.com>
2025-08-06 14:53:13 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HookGenerator from "../components/HookGenerator";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Welcome to Cloutline</h1>
+      <HookGenerator />
+    </div>
+  );
+}
